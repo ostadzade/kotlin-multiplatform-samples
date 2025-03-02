dc20eff41a1bb84291debc48c439c2dd154e72ee
ostadzade <m.nhi.co@gmail.com>
2025-03-02 13:43:51 +0330

    Update build.gradle.kts

--- Fruitties/androidApp/build.gradle.kts
+++ Fruitties/androidApp/build.gradle.kts
@@ -1,18 +1,3 @@
-/*
- * Copyright 2024 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.kotlinAndroid)
@@ -27,29 +12,8 @@ android {
         applicationId = "com.example.fruitties.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2 // افزایش versionCode
-        versionName = "1.1" // افزایش versionName
-
-        signingConfigs {
-            create("release") {
-                storeFile = file("release.keystore")
-                storePassword = "your_store_password"
-                keyAlias = "your_key_alias"
-                keyPassword = "your_key_password"
-            }
-        }
-
-        buildTypes {
-            getByName("release") {
-                signingConfig = signingConfigs.getByName("release")
-                isMinifyEnabled = true
-                isShrinkResources = true
-                proguardFiles(
-                    getDefaultProguardFile("proguard-android-optimize.txt"),
-                    "proguard-rules.pro"
-                )
-            }
-        }
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildFeatures {
@@ -59,6 +23,27 @@ android {
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file("release.keystore")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEY_PASSWORD")
+        }
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            signingConfig = signingConfigs.getByName("release")
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
 
